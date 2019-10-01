68eb8ba11115bb088a22bfcd28dd5bbd7186a3a9
Luis Weich <mambabosso@gmail.com>
2019-10-01 19:55:44 +0200

    Add files via upload

---
+++ WhereIsMyMoney/WhereIsMyMoney/Data/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace WhereIsMyMoney.Data
+{
+    [Serializable]
+    public sealed class Person
+    {
+        public string Id { get; private set; }
+
+        public DateTime CreationDateTime { get; private set; }
+
+        public string DisplayName { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Phone { get; private set; }
+
+        private Person()
+        {
+            Id = Guid.NewGuid().ToString("D");
+            CreationDateTime = DateTime.Now;
+        }
+
+        public Person(string displayName, string firstName, string lastName, string email, string phone) : this()
+        {
+            if (string.IsNullOrWhiteSpace(displayName))
+            {
+                throw new ArgumentNullException("displayName");
+            }
+            DisplayName = displayName;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+        }
+
+        public Person(string displayName, string firstName, string lastName) : this(displayName, firstName, lastName, string.Empty, string.Empty)
+        {
+        }
+    }
+}
